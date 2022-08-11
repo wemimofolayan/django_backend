c9ec361846aaf4a9ffbf018cb972907f9282c562
wemimofolayan <68654903+wemimofolayan@users.noreply.github.com>
2022-08-11 11:11:10 +0100

    Update managers.py
    removed the role of a global admin: 
    To run successfully, makemigrations and migrate

--- django_project/api/managers.py
+++ django_project/api/managers.py
@@ -30,7 +30,5 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        if extra_fields.get('role') != 1:
-            raise ValueError('Superuser must have role of Global Admin')
         
         return self.create_user(email, password, **extra_fields)
